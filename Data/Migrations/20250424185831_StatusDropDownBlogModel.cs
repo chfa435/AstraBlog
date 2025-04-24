343bced2732801a3b8e72afcf63eea0f41b92a89
bigbubba farm <farmercc@hotmail.com>
2025-04-24 15:00:24 -0400

    Migration for BlogPost model change with Status versus IsPublished?

---
+++ Data/Migrations/20250424185831_StatusDropDownBlogModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AstraBlog.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class StatusDropDownBlogModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPublished",
+                table: "BlogPosts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "BlogPosts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "BlogPosts");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublished",
+                table: "BlogPosts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
